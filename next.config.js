83e68ee6dff9ee0c97fd57006592849cca6c0046
Nourdine Labib <Nour-Dine-XD@outlook.com>
2022-04-02 00:01:34 +0000

    remove sv files, add sv files to .gitignore

--- next.config.js
+++ next.config.js
@@ -1,12 +1,17 @@
-/** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa');
 const runtimeCaching = require('next-pwa/cache');
 
 module.exports = withPWA({
+   reactStrictMode: true,
    pwa: {
-      disable: process.env.NODE_ENV === 'development',
       dest: 'public',
-      scope: '/',
+      register: true,
+      skipWaiting: true,
+      disable: process.env.NODE_ENV === 'development',
       runtimeCaching,
+   },
+   i18n: {
+      locales: ['en-US'],
+      defaultLocale: 'en-US',
    },
 });
