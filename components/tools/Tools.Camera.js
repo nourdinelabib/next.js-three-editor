be5bcd60268b2b73f6c75238835fb661d1e583c6
Nourdine Labib <Nour-Dine-XD@outlook.com>
2022-04-03 07:29:08 +0000

    add Camera section

---
+++ components/tools/Tools.Camera.js
@@ -0,0 +1,68 @@
+import * as THREE from 'three';
+
+import { UIPanel, UIButton, UIDiv } from '../../public/libs/ui.js';
+import { AddObjectCommand } from '../../commands/AddObjectCommand.js';
+
+function Camera(editor) {
+   const strings = editor.strings;
+
+   const container = new UIPanel();
+   container.setId('camera');
+
+   //* Camera
+
+   const history = new UIButton();
+   history.addClass('center');
+
+   const historyIcon = document.createElement('img');
+   historyIcon.alt = strings.getKey('camera');
+   historyIcon.src = 'images/camera.svg';
+
+   history.dom.appendChild(historyIcon);
+   container.add(history);
+
+   const options = new UIDiv();
+   options.setClass('options');
+   container.add(options);
+
+   const toolsOptions = new UIDiv();
+   toolsOptions.setClass('tools');
+   options.add(toolsOptions);
+
+   // PerspectiveCamera
+   const perspectivecamera = new UIButton();
+   perspectivecamera.addClass('center');
+
+   const perspectivecameraIcon = document.createElement('img');
+   perspectivecameraIcon.alt = strings.getKey('light/perspectivecamera');
+   perspectivecameraIcon.src = 'images/perspectivecamera.svg';
+   perspectivecamera.dom.appendChild(perspectivecameraIcon);
+   perspectivecamera.onClick(function () {
+      const camera = new THREE.PerspectiveCamera();
+      camera.name = 'PerspectiveCamera';
+
+      editor.execute(new AddObjectCommand(editor, camera));
+   });
+   toolsOptions.add(perspectivecamera);
+
+   // OrthographicCamera
+   const orthographiccamera = new UIButton();
+   orthographiccamera.addClass('center');
+
+   const orthographiccameraIcon = document.createElement('img');
+   orthographiccameraIcon.alt = strings.getKey('light/orthographiccamera');
+   orthographiccameraIcon.src = 'images/orthographiccamera.svg';
+   orthographiccamera.dom.appendChild(orthographiccameraIcon);
+   orthographiccamera.onClick(function () {
+      const aspect = editor.camera.aspect;
+      const camera = new THREE.OrthographicCamera(-aspect, aspect);
+      camera.name = 'OrthographicCamera';
+
+      editor.execute(new AddObjectCommand(editor, camera));
+   });
+   toolsOptions.add(orthographiccamera);
+
+   return container;
+}
+
+export { Camera };
--- components/tools/Tools.js
+++ components/tools/Tools.js
@@ -3,6 +3,7 @@ import { UIPanel } from '../../public/libs/ui.js';
 import { ToolsHistory } from './Tools.History.js';
 import { Mesh } from './Tools.Mesh.js';
 import { Light } from './Tools.Light.js';
+import { Camera } from './Tools.Camera.js';
 
 function Tools(editor) {
    const container = new UIPanel();
@@ -11,6 +12,7 @@ function Tools(editor) {
    container.add(new ToolsHistory(editor));
    container.add(new Mesh(editor));
    container.add(new Light(editor));
+   container.add(new Camera(editor));
 
    return container;
 }
