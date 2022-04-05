82c40a98401b128d4c150ac345488b9c14d584ca
Nourdine Labib <Nour-Dine-XD@outlook.com>
2022-04-05 04:29:29 +0000

    add RemoveLastLine func

--- components/tools/Tools.ContinuousLines.js
+++ components/tools/Tools.ContinuousLines.js
@@ -2,7 +2,6 @@ import * as THREE from 'three';
 
 import { UIPanel, UIButton } from '../../public/libs/ui.js';
 import { AddObjectCommand } from '../../commands/AddObjectCommand.js';
-import { RemoveObjectCommand } from '../../commands/RemoveObjectCommand.js';
 
 function ContinuousLines(editor) {
    const strings = editor.strings;
@@ -16,8 +15,15 @@ function ContinuousLines(editor) {
       e.stopPropagation();
    });
 
-   //* lines
+   //* Camera
+   const aspect = editor.camera.aspect;
+   const camera = new THREE.OrthographicCamera(-aspect, aspect);
+   camera.name = 'Drowing Camera';
+   camera.position.set(0, 0, 1);
 
+   editor.viewportCamera = editor.camera;
+
+   //* lines
    const lines = new UIButton();
    lines.addClass('center');
 
@@ -29,7 +35,7 @@ function ContinuousLines(editor) {
 
    let active = false;
 
-   let MAX_POINTS = 500;
+   let MAX_POINTS = 150;
    const mouse = new THREE.Vector2();
 
    // geometry
@@ -39,17 +45,14 @@ function ContinuousLines(editor) {
    let positions = new Float32Array(MAX_POINTS * 3); // 3 vertices per point
    geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
 
-   // draw range
-   let drawCount = 2; // draw the first 2 points, only
-   geometry.setDrawRange(0, drawCount);
+   // // draw range
+   // let drawCount = 1; // draw the first 2 points, only
+   // geometry.setDrawRange(0, drawCount);
 
    // material
    var material = new THREE.LineBasicMaterial({
       color: 0xff0000,
-      side: THREE.DoubleSide,
-      wireframe: true,
-      // depthTest: false,
-      blending: THREE.NormalBlending,
+      linewidth: 1,
    });
 
    // line
@@ -62,14 +65,6 @@ function ContinuousLines(editor) {
    let y = 0;
    let z = 0;
    let index = 0;
-
-   // Camera
-   const aspect = editor.camera.aspect;
-   const camera = new THREE.OrthographicCamera(-aspect, aspect);
-   camera.name = 'Drowing Camera';
-   camera.position.set(0, 0, 1);
-
-   editor.viewportCamera = editor.camera;
 
    lines.onClick(function () {
       if (!active) {
@@ -84,6 +79,7 @@ function ContinuousLines(editor) {
          lines.removeClass('selected');
          document.removeEventListener('mousemove', onMouseMove, false);
          document.removeEventListener('mousedown', addPoint, false);
+         RemoveLastLine();
 
          editor.viewportCamera = editor.camera;
       }
@@ -103,8 +99,7 @@ function ContinuousLines(editor) {
       mouse.x = (event.clientX / viewport.offsetWidth) * 2 - 1;
       mouse.y = -(event.clientY / viewport.offsetHeight) * 2 + 1;
       mouse.z = 0;
-      //mouse.unproject(camera);
-      if (drawCount !== 0) {
+      if (index > 0) {
          updateLine();
       }
    }
@@ -114,9 +109,21 @@ function ContinuousLines(editor) {
       positions[index * 3 + 0] = mouse.x;
       positions[index * 3 + 1] = mouse.y;
       positions[index * 3 + 2] = mouse.z;
+
       index++;
       line.geometry.setDrawRange(0, index);
+
       updateLine();
+   }
+
+   // update line
+   function RemoveLastLine() {
+      positions[index * 3 - 3] = positions[(index - 1) * 3 - 3];
+      positions[index * 3 - 2] = positions[(index - 1) * 3 - 2];
+      positions[index * 3 - 1] = positions[(index - 1) * 3 - 1];
+
+      editor.execute(new AddObjectCommand(editor, line));
+      line.geometry.attributes.position.needsUpdate = true;
    }
 
    container.add(lines);
