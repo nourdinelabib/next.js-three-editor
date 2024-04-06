c59dbba60a857e71ae7f2eb5f6192f521c35fd63
4rtb <141040913+4rtb@users.noreply.github.com>
2024-04-06 23:16:42 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
-const withPWA = require('next-pwa');
-const runtimeCaching = require('next-pwa/cache');
+const withPWA = require('next-pwa')({
+  dest: 'public'
+})
 
 module.exports = withPWA({
-   reactStrictMode: true,
-   pwa: {
-      dest: 'public',
-      register: true,
-      skipWaiting: true,
-      disable: process.env.NODE_ENV === 'development',
-      runtimeCaching,
-   },
-});
+  // next.js config
+})
